5c2fb21cb72f4b529f8c808e19ad5c6caa3848a7
Catalin Chiorean <catalinchiorean@3pillarglobal.com>
2022-07-11 22:39:20 +0300

    Added a check that validates that when you click on the “Conduit” logo in the top left of the site, it returns to the homepage

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -8,8 +8,14 @@ describe('Homepage', function () {
 
     // Click the 'Sign in' navigation link
     $('=Sign in').click();
-    
+
     // Get the URL of the sign in page. It should include 'login'
     expect(browser).toHaveUrl('/login', { containing: true });
+
+    // Click the 'Conduit' logo
+    $('=conduit').click();
+
+    // Get the URL of the sign in page. It should be the homepage
+    expect(browser).toHaveUrl('', { containing: true });
   });
 });
